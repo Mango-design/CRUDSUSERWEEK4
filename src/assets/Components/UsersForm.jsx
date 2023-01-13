830722d0e66249cc0ea953ad1526e3f9ad35f20f
Mango-design <angelclash21@gmail.com>
2023-01-13 15:56:42 -0600

    version bugeada

--- src/assets/Components/UsersForm.jsx
+++ src/assets/Components/UsersForm.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const UsersForm = () => {
     
+    const {handleSubmit, register} = useForm()
+
+    const submit =(data) =>{
+        console.log(data);
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit(submit)}>
             <div className="input-container">
                 <label htmlFor="email">Email</label>
-                <input type="text" id='email' />
+                <input type="text" id='email' {...register("email")} />
             </div>
             <div className="input-container">
                 <label htmlFor="password">Password</label>
-                <input type="text" id='password' />
+                <input type="text" id='password' {...register("password")} />
             </div>
             <div className="input-container">
                 <label htmlFor="first_name">First-Name</label>
-                <input type="text" id='first-name' />
+                <input type="text" id='first-name' {...register("first_name")} />
             </div>
             <div className="input-container">
                 <label htmlFor="last_name">last-name</label>
-                <input type="text" id='last_name' />
+                <input type="text" id='last_name' {...register("last_name")} />
             </div>
             <div className="input-container">
                 <label htmlFor="birthday">Birthday</label>
-                <input type="date" id='birthday' />
+                <input type="date" id='birthday' {...register("birthday")} />   
             </div>
+            <button>submit</button>
         </form>
     );
 };
