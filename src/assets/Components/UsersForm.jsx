4561efe99cb9270478a94e3d89d76547f6a11525
CarolinaVlr <dianacarolinav97@gmail.com>
2023-01-13 17:07:07 -0500

    space placeholder

--- src/assets/Components/UsersForm.jsx
+++ src/assets/Components/UsersForm.jsx
@@ -9,23 +9,29 @@ const UsersForm = () => {
                 <h1>Nuevo Usuario</h1>
             <div className="input-container">
                 <label htmlFor="email">Email</label>
-                <input type="text" id='email' placeholder='Email'/>
+                <input type="text" id='email' 
+                placeholder='Email
+                '/>
             </div>
             <div className="input-container">
                 <label htmlFor="password">Password</label>
-                <input type="text" id='password' placeholder='Password' />
+                <input type="text" id='password' 
+                placeholder='Password' />
             </div>
             <div className="input-container">
                 <label htmlFor="first_name">First-Name</label>
-                <input type="text" id='first-name' placeholder='First-Name' />
+                <input type="text" id='first-name' 
+                placeholder='First-Name' />
             </div>
             <div className="input-container">
                 <label htmlFor="last_name">Last-name</label>
-                <input type="text" id='last_name' placeholder='Last-Name' />
+                <input type="text" id='last_name'
+                 placeholder='Last-Name' />
             </div>
             <div className="input-container">
                 <label htmlFor="birthday">Birthday</label>
-                <input type="date" id='birthday' placeholder='Birthday' />
+                <input type="date" id='birthday'
+                 placeholder='Birthday' />
             </div>
             </div>
             
