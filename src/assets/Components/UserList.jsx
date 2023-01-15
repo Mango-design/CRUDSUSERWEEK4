633eef3a1427d42a413d02ce58d8e54d7cf69d96
CarolinaVlr <dianacarolinav97@gmail.com>
2023-01-14 20:15:34 -0500

    desig botons

--- src/assets/Components/UserList.jsx
+++ src/assets/Components/UserList.jsx
@@ -6,12 +6,10 @@ const UserList = ({ userList, selectUser, setIsVisible }) => {
     const lastIndex = userForPage * page
     const firstIndex = lastIndex - userForPage
     const userPagined = userList.slice(firstIndex,lastIndex)
-    const totalPage = Math.ceil(userList/userForPage)
-    const pageNumbers = []
-    for (let i = 0; i<=totalPage;i++){
-        pageNumbers.push(i)
-    }
+    const totalpage = Math.ceil(userList.length / userForPage)   
 
+
+ 
     const visible = () =>{
         setIsVisible(true)
     }
@@ -24,7 +22,11 @@ const UserList = ({ userList, selectUser, setIsVisible }) => {
         <div className='userList'>
             <div className='userList_container_title'>
                 <h1>UserList</h1>
+                <button className='page' onClick={()=>{setPage(page-1)}} disabled={page===1} >Prev Page</button>
+               
+                <button className='page' onClick={()=>{setPage(page+1)}} disabled={page === totalpage}>Next Page</button>
                 <button onClick={()=>{setIsVisible(true)}} className='create'><i className="fa-solid fa-plus"></i> Crear Nuevo Usuario</button>
+                
             </div>
 
             <ul>
